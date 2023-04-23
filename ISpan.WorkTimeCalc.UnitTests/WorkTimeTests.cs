f6ca1c13710dad44914c76f6dd8358d1f8fd49c8
TedYin <jjboy830227@gmail.com>
2023-04-23 12:54:17 +0800

    修改計算方式及新增Datetime格式

--- ISpan.WorkTimeCalc.UnitTests/WorkTimeTests.cs
+++ ISpan.WorkTimeCalc.UnitTests/WorkTimeTests.cs
@@ -2,48 +2,96 @@ using NUnit.Framework.Internal.Execution;
 
 namespace ISpan.WorkTimeCalc.UnitTests
 {
-	public class WorkTimeTests
-	{
+    public class WorkTimeTests
+    {
 
 
-		[Test]
-		public void LeaveHoursCalc_計算請假時間_9點至18點()
-		{
-			WorkTime workTime = new WorkTime();
-			int actual = workTime.LeaveHoursCalc(9, 18);
-			Assert.AreEqual(8, actual);
-		}
+        [Test]
+        public void LeaveHoursCalc_計算請假時間_9點至18點()
+        {
+            WorkTime workTime = new WorkTime();
+            double actual = workTime.LeaveHoursCalc(9, 18);
+            Assert.AreEqual(8, actual);
+        }
 
-		[Test]
-		public void LeaveHoursCalc_計算請假時間_6點至23點()
-		{
-			WorkTime workTime = new WorkTime();
-			int actual = workTime.LeaveHoursCalc(6, 23);
-			Assert.AreEqual(8, actual);
-		}
+        [Test]
+        public void LeaveHoursCalc_計算請假時間_6點至23點()
+        {
+            WorkTime workTime = new WorkTime();
+            double actual = workTime.LeaveHoursCalc(6, 23);
+            Assert.AreEqual(8, actual);
+        }
 
-		[Test]
-		public void LeaveHoursCalc_計算請假時間_12點至18點()
-		{
-			WorkTime workTime = new WorkTime();
-			int actual = workTime.LeaveHoursCalc(12, 18);
-			Assert.AreEqual(5, actual);
-		}
+        [Test]
+        public void LeaveHoursCalc_計算請假時間_12點至18點()
+        {
+            WorkTime workTime = new WorkTime();
+            double actual = workTime.LeaveHoursCalc(12, 18);
+            Assert.AreEqual(5, actual);
+        }
 
-		[Test]
-		public void LeaveHoursCalc_計算請假時間_9點至11點()
-		{
-			WorkTime workTime = new WorkTime();
-			int actual = workTime.LeaveHoursCalc(9, 11);
-			Assert.AreEqual(2, actual);
-		}
+        [Test]
+        public void LeaveHoursCalc_計算請假時間_9點至11點()
+        {
+            WorkTime workTime = new WorkTime();
+            double actual = workTime.LeaveHoursCalc(9, 11);
+            Assert.AreEqual(2, actual);
+        }
 
-		[Test]
-		public void LeaveHoursCalc_計算請假時間_12點至25點()
-		{
-			WorkTime workTime = new WorkTime();
-			int actual = workTime.LeaveHoursCalc(12, 25);
-			Assert.AreEqual(-1, actual);
-		}
-	}
+        [Test]
+        public void LeaveHoursCalc_計算請假時間_錯誤的時間範圍()
+        {
+            WorkTime workTime = new WorkTime();
+            double actual = workTime.LeaveHoursCalc(12, int.MaxValue);
+            Assert.AreEqual(-1, actual);
+        }
+
+        [Test]
+        public void LeaveHoursCalc_計算請假時間_工時範圍外()
+        {
+            WorkTime workTime = new WorkTime();
+            double actual = workTime.LeaveHoursCalc(6, 7);
+            Assert.AreEqual(0, actual);
+        }
+
+
+        [Test]
+        public void LeaveHoursCalc_計算請假時間_DateTime_1天以內()
+        {
+            DateTime dt1 = new DateTime(2023, 04, 10, 9, 00, 00);
+            DateTime dt2 = new DateTime(2023, 04, 10, 18, 00, 00);
+            WorkTime workTime = new WorkTime();
+            double actual = workTime.LeaveHoursCalc(dt1, dt2);
+            Assert.AreEqual(8, actual);
+        }
+
+        [Test]
+        public void LeaveHoursCalc_計算請假時間_DateTime_3天()
+        {
+            DateTime dt1 = new DateTime(2023, 04, 10, 9, 00, 00);
+            DateTime dt2 = new DateTime(2023, 04, 12, 18, 00, 00);
+            WorkTime workTime = new WorkTime();
+            double actual = workTime.LeaveHoursCalc(dt1, dt2);
+            Assert.AreEqual(24, actual);
+        }
+
+        [Test]
+        public void LeaveHoursCalc_計算請假時間_DateTime_3天_9點到9點()
+        {
+            DateTime dt1 = new DateTime(2023, 04, 10, 9, 00, 00);
+            DateTime dt2 = new DateTime(2023, 04, 12, 9, 00, 00);
+            WorkTime workTime = new WorkTime();
+            double actual = workTime.LeaveHoursCalc(dt1, dt2);
+            Assert.AreEqual(16, actual);
+        }
+        [Test]
+        public void LeaveHoursCalc_計算請假時間_DateTime_結束時間小於開始時間()
+        {
+            DateTime dt1 = new DateTime(2023, 04, 10, 9, 00, 00);
+            DateTime dt2 = new DateTime(2023, 04, 09, 9, 00, 00);
+            WorkTime workTime = new WorkTime();
+            double actual = workTime.LeaveHoursCalc(dt1, dt2);
+            Assert.AreEqual(-1, actual);
+        }
+    }
 }
--- ISpan.WorkTimeCalc/WorkTime.cs
+++ ISpan.WorkTimeCalc/WorkTime.cs
@@ -1,87 +1,153 @@
 ﻿namespace ISpan.WorkTimeCalc
 {
-	public class WorkTime
-	{
-		// 定義上班時間
-		private const int workBegin = 9;
-		// 定義下班時間
-		private const int workEnd = 18;
+    public class WorkTime
+    {
+        // 上班時間
+        private const int workBegin = 9;
+        // 下班時間
+        private const int workEnd = 18;
 
-		// 將需要扣除的休息時間放入陣列
-		private int[] _restTimes = { 12 };
-		private List<int> RestTime => _restTimes.ToList();
-		/// <summary>
-		/// 獲得請假時間資訊
-		/// </summary>
-		/// <param name="leaveBegin">請假開始時間</param>
-		/// <param name="leaveEnd">請假結束時間</param>
-		/// <returns></returns>
-		public string GetLeaveHoursInfo(int leaveBegin, int leaveEnd)
-		{
-			int result = LeaveHoursCalc(leaveBegin, leaveEnd);
-			return result < 0
-				? "輸入的時間不正確，請重新輸入"
-				: $"請假的時間 從 {leaveBegin,2} 至 {leaveEnd,2} 為: {result} 小時";
-		}
+        // 開始休息時間
+        private const double RestBegin = 12;
+        // 結束休息時間
+        private const double RestEnd = 13;
+        /// <summary>
+        /// 獲得請假時間資訊
+        /// </summary>
+        /// <param name="leaveBegin">請假開始時間</param>
+        /// <param name="leaveEnd">請假結束時間</param>
+        /// <returns></returns>
+        public string GetLeaveHoursInfo(int leaveBegin, int leaveEnd)
+        {
+            double result = LeaveHoursCalc(leaveBegin, leaveEnd);
+            return result < 0
+                ? "請輸入正確起始及結束時間"
+                : $"請假的時間 從 {leaveBegin,2} 至 {leaveEnd,2} 為: {result} 小時";
+        }
+        /// <summary>
+        /// 計算請假時數
+        /// </summary>
+        /// <param name="leaveBegin">請假開始時間</param>
+        /// <param name="leaveEnd">請假結束時間</param>
+        /// <returns></returns>
+        public double LeaveHoursCalc(DateTime leaveBegin, DateTime leaveEnd)
+        {
+            // 驗證開始、結束時間，不合法即回傳 -1
+            if (!IsReasonableTime(leaveBegin, leaveEnd)) return -1;
 
-		/// <summary>
-		/// 計算請假時數
-		/// </summary>
-		/// <param name="leaveBegin">請假開始時間</param>
-		/// <param name="leaveEnd">請假結束時間</param>
-		/// <returns></returns>
-		public int LeaveHoursCalc(int leaveBegin, int leaveEnd)
-		{
-			// 驗證開始、結束時間，不合法即回傳 -1
-			if (!IsReasonableTime(leaveBegin, leaveEnd)) return -1;
+            // 算出傳入日期橫跨了多少天
+            TimeSpan ts = leaveEnd.Date - leaveBegin.Date;
+            int totaldays = (int)Math.Ceiling(ts.TotalDays);
 
-			leaveBegin = Math.Max(leaveBegin, workBegin); // 如果開始時間小於上班時間，等於上班時間
-			leaveEnd = Math.Min(leaveEnd, workEnd); // 如果結束時間大於下班時間，等於下班時間
+            DateTime leaveBeginOfDate;
+            DateTime leaveEndOfDate;
 
-			// 獲得一個正式請假的時間段清單(過濾掉休息時間)
-			List<int> leaveHours = HoursToList(leaveBegin, leaveEnd, RestTime);
+            // 儲存總時數的變數
+            double leaveHours = 0;
 
-			return leaveHours.Count;
-		}
+            // 計算每天時間
+            for (int i = 0; i <= totaldays; i++)
+            {
+                // 如果計算的不是第一天，起始時間為0:00
+                leaveBeginOfDate = i == 0 ? leaveBegin : leaveBegin.AddDays(i).Date;
+                // 如果計算的不是最後一天，結束時間為24:00
+                leaveEndOfDate = i == totaldays ? leaveEnd : leaveBeginOfDate.AddDays(1).Date;
 
-		/// <summary>
-		/// 將時間過濾後放入列表
-		/// </summary>
-		/// <param name="begin">開始時間</param>
-		/// <param name="end">結束時間</param>
-		/// <param name="filterList">要過濾掉的時間段</param>
-		/// <returns></returns>
-		private List<int> HoursToList(int begin, int end, List<int>? filterList = null)
-		{
-			var hourList = new List<int>();
-			filterList = filterList ?? new List<int>();
+                // 當天幾點開始
+                double begin = (leaveBeginOfDate - leaveBeginOfDate.Date).TotalHours;
+                // 當天幾點結束
+                double end = (leaveEndOfDate - leaveBeginOfDate.Date).TotalHours;
 
-			for (int i = begin; i < end; i++)
-			{
-				// 如果此時間為需要過濾掉的時間，直接進入下一圈，不添加此時間
-				if (filterList.Contains(i)) continue;
+                // 計算當天的總時數
+                leaveHours += LeaveHoursCalc(begin, end);
+            }
 
-				hourList.Add(i);
-			}
+            return leaveHours;
+        }
 
-			return hourList;
-		}
+        /// <summary>
+        /// 計算請假時數
+        /// </summary>
+        /// <param name="leaveBegin">請假開始時間</param>
+        /// <param name="leaveEnd">請假結束時間</param>
+        /// <returns></returns>
+        public double LeaveHoursCalc(double leaveBegin, double leaveEnd)
+        {
+            // 驗證開始、結束時間，不合法即回傳 -1
+            if (!IsReasonableTime(leaveBegin, leaveEnd)) return -1;
 
-		/// <summary>
-		/// 驗證時間數字是否合法
-		/// </summary>
-		/// <param name="begin">開始時間</param>
-		/// <param name="end">結束時間</param>
-		/// <returns></returns>
-		private bool IsReasonableTime(int begin, int end)
-		{
-			if (begin < 0
-				|| end < 0
-				|| begin > 24
-				|| end > 24
-				|| begin > end) return false;
+            // 將需計算的開始及結束時間，限制在上班時間段裡
+            leaveBegin = WorkTimeBondary(leaveBegin);
+            leaveEnd = WorkTimeBondary(leaveEnd);
 
-			else return true;
-		}
-	}
+            // 如果請假開始時間在休息範圍時間內，重設在休息時間開始時
+            leaveBegin = IsInRange(leaveBegin, RestBegin, RestEnd) ? RestBegin : leaveBegin;
+            // 如果請假結束時間在休息範圍時間內，重設在休息時間結束時
+            leaveEnd = IsInRange(leaveEnd, RestBegin, RestEnd) ? RestEnd : leaveEnd;
+
+            // 回傳總請假時數
+            return HoursCalcWithoutRange(leaveBegin, leaveEnd, RestBegin, RestEnd);
+
+        }
+        /// <summary>
+        /// 獲得去除掉指定範圍的時數
+        /// </summary>
+        /// <param name="timeBegin">起始時間</param>
+        /// <param name="timeEnd">結束時間</param>
+        /// <param name="rangeBegin">去除範圍時間起始</param>
+        /// <param name="rangeEnd">去除範圍時間結束</param>
+        /// <returns></returns>
+        private double HoursCalcWithoutRange(double timeBegin, double timeEnd, double rangeBegin, double rangeEnd)
+        {
+            double totalhour = timeEnd - timeBegin;
+
+            // 如果時間段包含要去除的範圍，則扣除
+            if (IsInRange(rangeBegin, timeBegin, timeEnd) && IsInRange(rangeEnd, timeBegin, timeEnd))
+                return Math.Round(totalhour - (rangeEnd - rangeBegin), 1, MidpointRounding.AwayFromZero);
+
+            return Math.Round(totalhour, 1, MidpointRounding.AwayFromZero);
+        }
+
+        /// <summary>
+        /// 驗證時間數字是否合法
+        /// </summary>
+        /// <param name="begin">開始時間</param>
+        /// <param name="end">結束時間</param>
+        /// <returns></returns>
+        private bool IsReasonableTime(DateTime begin, DateTime end) => begin <= end;
+
+        /// <summary>
+        /// 驗證時間數字是否合法
+        /// </summary>
+        /// <param name="begin">開始時間</param>
+        /// <param name="end">結束時間</param>
+        /// <returns></returns>
+        private bool IsReasonableTime(double begin, double end)
+        {
+            if (begin < 0 || end < 0 || begin > 24 || end > 24) return false;
+
+            return begin <= end;
+        }
+
+        /// <summary>
+        /// 將時間限制在上班時間以內
+        /// </summary>
+        /// <param name="time">時間</param>
+        /// <returns></returns>
+        private double WorkTimeBondary(double time)
+        {
+            time = Math.Max(time, workBegin); // 如果時間小於上班時間，等於上班時間
+            time = Math.Min(time, workEnd); // 如果時間大於下班時間，等於下班時間
+
+            return time;
+        }
+        /// <summary>
+        /// 判斷參數是否在範圍內
+        /// </summary>
+        /// <param name="time">參數</param>
+        /// <param name="rangeBegin">範圍起始</param>
+        /// <param name="rangeEnd">範圍結束</param>
+        /// <returns></returns>
+        private bool IsInRange(double time, double rangeBegin, double rangeEnd) => time >= rangeBegin && time <= rangeEnd;
+    }
 }
